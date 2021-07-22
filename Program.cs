51ee759d357a517399892df75d8f4a3b23a6d175
Hyaku Ju Yon <ichitora114@outlook.com>
2021-07-22 05:17:53 +0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Mod_4._3._2_Pract
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int[,] array = { { 1, 2, 3 }, { 5, 6, 7 } };
+            
+            foreach(var item in array)
+
+                {
+
+                Console.Write(item + "");
+
+                }
+
+
+        }
+    }
+}
